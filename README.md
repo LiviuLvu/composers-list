b12d76ed1b7d69622dff42f434cc7a3daa776af8
Liviu <lvudesign@gmail.com>
2016-06-20 01:02:35 +0300

    Resolved connection between list.js and JSON
    sorting and searching is working

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ Sortable, searchable list of classical composers.
 Loading existing data with Jquerry from a .json file
 The data manipulation will be made using list.js
 
+Preview Image:
+![image](https://github.com/LiviuLvu/canvas-clock/blob/master/composers-list.jpg)  
+
+
+test app online here:  
+http://lvudesign.com/composers-list/  
+
 #### Work in progress  
--- js/jsonTest.js
+++ js/jsonTest.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
-   $('#get-data').click(function() {
-      var showData = $('#show-data');
+   // populate empty list in the html
+   var showData = $('#show-data');
 
-      $.getJSON('data/composers.json', function(data) {
-         // how does map work?
-         var items = data.map(function(data) {
-            return data.name + ' - ' + data.born;
-         });
-         // removes dom elements and text content inside selected element
-         showData.empty();
-         // if list is not empty
-         if (items.length) {
-            var content = '<li>' + items.join('</li><li>') + '</li>';
-            var list = $('<ul />').html(content);
-            // insert data inside selected element
-            showData.append(list);
-         }
-      });
+   $.getJSON('data/composers.json', function(data) {
+      // Using list.js below
+      // Define value names
+      var options = {
+         valueNames: ['name', 'born', 'died', 'nationality'],
+         item: '<li><h5 class="name"></h5><span class="born"></span> - <span class="died"></span> <p class="nationality"></p> </li>'
+      };
 
-      showData.text('Loading the JSON file.');
+      var values = data;
+
+      // Init list
+      var userList = new List('users', options, values);
+
    });
+   // waiting message when data is loading slow
+   showData.text('Loading the JSON file.');
 });
