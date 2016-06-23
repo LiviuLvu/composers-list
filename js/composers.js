1217febda079c6cdd02bb70ffecc5af860c4430f
Liviu <liviuiancu@gmx.com>
2016-06-24 02:03:03 +0300

    Added images for profile
    Using custom attributes from list.js

--- js/composers.js
+++ js/composers.js
@@ -8,9 +8,12 @@ $(document).ready(function() {
       var values = data;
       // Define value names
       var options = {
-         valueNames: ['name', 'born', 'died', 'nationality', 'profile'],
+         valueNames: ['name', 'born', 'died', 'nationality', 'profile', { name: 'image', attr: 'src' }],
          plugins: [ListFuzzySearch()],
-         item: '<li><h5 class="name"></h5><span class="born"></span> - <span class="died"></span> <p class="nationality"></p> <span class="profile"></span></li>'
+         item: '<li><h5 class="name"></h5><span class="born">' +
+            '</span> - <span class="died"></span>' +
+            '<p class="nationality"></p> <img class="image" src="img/alexander-borodin.jpg" alt="Composer Image">' +
+            '<p class="profile"></p></li>'
       };
       // Init list
       var userList = new List('users', options, values);
