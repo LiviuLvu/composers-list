8548abdef6b45081a0d3fbf70988a9c992f4f1d6
Liviu <liviuiancu@gmx.com>
2016-06-25 00:14:09 +0300

    Audio sample is available for each player
    Needs fix:
    Stop current play if user clicks another play button

--- js/composers.js
+++ js/composers.js
@@ -8,14 +8,16 @@ $(document).ready(function() {
       var values = data;
       // Define value names
       var options = {
-         valueNames: ['name', 'born', 'died', 'nationality', 'profile', { name: 'image', attr: 'src' }],
+         valueNames: ['name', 'born', 'died', 'nationality', 'profile', { name: 'image', attr: 'src' }, { name: 'audiosample', attr: 'src' }],
          plugins: [ListFuzzySearch()],
          item: '<li><h5 class="name"></h5><span class="born">' +
             '</span> - <span class="died"></span>' +
             '<p class="nationality"></p> <img class="image" src="img/alexander-borodin.jpg" alt="Composer Image">' +
+            '<audio controls class="audiosample" src="audio/mozart-eine-kleine-nachtmusik.mp3" type="audio/mpeg">' +
+            'Audio not supported, please update browser</audio>' +
             '<p class="profile"></p></li>'
       };
-      // Init list
+      // initialize list
       var userList = new List('users', options, values);
 
    });
