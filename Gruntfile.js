04180b2692aab280df6788f54f3b152f60a62430
Liviu <liviuiancu@gmx.com>
2016-06-27 00:38:22 +0300

    Updated image for demo

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+   // Project configuration.
+   grunt.initConfig({
+      // Task configuration.
+      browserSync: {
+         bsFiles: {
+            src: ['js/*.js', 'index.html', 'css/*.css']
+         },
+         options: {
+            server: {
+               baseDir: "g:/node/http-server/public/GitHub/composers-list/"
+            }
+         }
+      }
+   });
+
+   // These plugins provide necessary tasks.
+   grunt.loadNpmTasks('grunt-browser-sync');
+
+   // Default task.
+   grunt.registerTask('default', ['browserSync']);
+
+};
